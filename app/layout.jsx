e5aed0961b9aac02baf39ff662edc122eef4f919
coderdal <agircoder@gmail.com>
2023-04-24 23:06:20 +0300

    header created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 
+import Header from "@/components/header.jsx";
+
 export const metadata = {
   title: "Workout Trainer",
   description:
@@ -12,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
