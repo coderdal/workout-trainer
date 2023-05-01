8f9d34b000a2b4ec9bc3644da52bd8df26e954a4
coderdal <agircoder@gmail.com>
2023-05-01 16:31:14 +0300

    header design

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import styles from "@/styles/header.module.css";
+import Link from "next/link";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <h3 className={styles.title}>
+        <Link href={"/"}>
+          Workout <span>Trainer</span>
+        </Link>
+      </h3>
+    </header>
+  );
 };
 
 export default Header;
